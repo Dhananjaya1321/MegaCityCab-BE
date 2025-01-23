6f0c93985a21195258719901e4f79cd8d20d1399
Dhananjaya1321 <blackisurudananjaya@gmail.com>
2025-01-23 22:02:09 +0530

    Created Driver entity

---
+++ src/main/java/com/megacitycab/megacitycab/entity/Driver.java
@@ -0,0 +1,42 @@
+package com.megacitycab.megacitycab.entity;
+
+import com.megacitycab.megacitycab.enums.DriverStatus;
+import com.megacitycab.megacitycab.enums.UserStatus;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Data
+@ToString
+@AllArgsConstructor
+@NoArgsConstructor
+@EntityListeners(AuditingEntityListener.class)
+public class Driver {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+    private String name;
+    private String nic;
+    private String address;
+    private String contact;
+
+    @Enumerated(EnumType.STRING)
+    private DriverStatus status;
+
+    private String createdBy;
+    private String updatedBy;
+
+    @LastModifiedDate
+    private LocalDateTime lastUpdate;
+
+    @OneToOne(cascade = CascadeType.ALL)
+    private User user;
+
+}
